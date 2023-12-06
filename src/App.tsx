e8551b3b4771d6de2742072b4ee94bb8514f9441
EijiGorilla <emasuz@hotmail.com>
2023-12-06 08:36:10 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,204 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useRef, useEffect, useState } from 'react';
+import { map, view, basemaps, layerList } from './Scene';
+import './index.css';
 import './App.css';
+import '@esri/calcite-components/dist/components/calcite-shell';
+import '@esri/calcite-components/dist/components/calcite-list';
+import '@esri/calcite-components/dist/components/calcite-list-item';
+import '@esri/calcite-components/dist/components/calcite-shell-panel';
+import '@esri/calcite-components/dist/components/calcite-action';
+import '@esri/calcite-components/dist/components/calcite-action-bar';
+import '@esri/calcite-components/dist/calcite/calcite.css';
+import {
+  CalciteShell,
+  CalciteShellPanel,
+  CalciteActionBar,
+  CalciteAction,
+  CalciteTabs,
+  CalcitePanel,
+  CalciteList,
+  CalciteListItem,
+} from '@esri/calcite-components-react';
+import Chart from './components/Chart';
 
 function App() {
+  //**** Set states */
+  const mapDiv = useRef(null);
+  const layerListDiv = useRef<HTMLDivElement | undefined | any>(null);
+
+  // For Calcite Design
+  const calcitePanelBasemaps = useRef<HTMLDivElement | undefined | any>(null);
+  const [activeWidget, setActiveWidget] = useState<undefined | any | unknown>(null);
+  const [nextWidget, setNextWidget] = useState<undefined | any | unknown>(null);
+
+  // Add zoomToLayer in App component, not LotChart component
+  useEffect(() => {
+    // zoomToLayer(lotLayer);
+  }, []);
+
+  // End of dropdown list
+  useEffect(() => {
+    if (activeWidget) {
+      const actionActiveWidget = document.querySelector(
+        `[data-panel-id=${activeWidget}]`,
+      ) as HTMLCalcitePanelElement;
+      actionActiveWidget.hidden = true;
+    }
+
+    if (nextWidget !== activeWidget) {
+      const actionNextWidget = document.querySelector(
+        `[data-panel-id=${nextWidget}]`,
+      ) as HTMLCalcitePanelElement;
+      actionNextWidget.hidden = false;
+    }
+  });
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      /**
+       * Initialize
+       */
+
+      map.ground.navigationConstraint = {
+        type: 'none',
+      };
+
+      view.container = mapDiv.current;
+      view.ui.components = [];
+      view.ui.empty('top-left');
+      basemaps.container = calcitePanelBasemaps.current;
+      layerList.container = layerListDiv.current;
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <CalciteShell>
+        <CalciteTabs slot="panel-end" style={{ width: '27vw' }}>
+          <Chart />
+        </CalciteTabs>
+        <header
+          slot="header"
+          id="header-title"
+          style={{ display: 'flex', width: '100%', padding: '0 1rem' }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src="https://EijiGorilla.github.io/Symbols/Projec_Logo/DOTr_Logo_v2.png"
+            alt="DOTr Logo"
+            height={'2.9%'}
+            width={'2.9%'}
+            style={{ marginBottom: 'auto', marginTop: 'auto' }}
+          />
+          <b className="headerTitle">SC ENVIRONMENT MONITORING</b>
+          <div className="date">As of August 1, 2023</div>
+
+          <img
+            src="https://EijiGorilla.github.io/Symbols/Projec_Logo/GCR LOGO.png"
+            alt="GCR Logo"
+            height={'4.4%'}
+            width={'4.4%'}
+            style={{
+              marginBottom: 'auto',
+              marginTop: 'auto',
+              marginLeft: 'auto',
+              marginRight: '5rem',
+            }}
+          />
+        </header>
+
+        <CalciteShellPanel
+          width-scale="1"
+          slot="panel-start"
+          position="start"
+          id="left-shell-panel"
+          displayMode="dock"
+        >
+          <CalciteActionBar slot="action-bar">
+            <CalciteAction
+              data-action-id="layers"
+              icon="layers"
+              text="layers"
+              id="layers"
+              //textEnabled={true}
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+
+            <CalciteAction
+              data-action-id="basemaps"
+              icon="basemap"
+              text="basemaps"
+              id="basemaps"
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+
+            <CalciteAction
+              data-action-id="information"
+              icon="information"
+              text="Information"
+              id="information"
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+          </CalciteActionBar>
+
+          <CalcitePanel
+            heading="Layers"
+            height-scale="l"
+            width-scale="l"
+            data-panel-id="layers"
+            style={{ width: '18vw' }}
+            hidden
+          >
+            <CalciteList>
+              <CalciteListItem
+                label=""
+                description=""
+                value="land-acquisition"
+                ref={layerListDiv}
+              ></CalciteListItem>
+            </CalciteList>
+          </CalcitePanel>
+
+          <CalcitePanel
+            heading="Basemaps"
+            height-scale="l"
+            data-panel-id="basemaps"
+            style={{ width: '18vw' }}
+            hidden
+          >
+            <CalciteList>
+              <CalciteListItem value="basemaps" ref={calcitePanelBasemaps}></CalciteListItem>
+            </CalciteList>
+          </CalcitePanel>
+
+          <CalcitePanel heading="Description" data-panel-id="information" hidden>
+            {nextWidget === 'information' ? (
+              <div className="informationDiv">
+                <ul>
+                  <li>
+                    <b>Chart</b> shows the number of monitoring stations in exceed of the allowable
+                    threshold values.
+                  </li>
+                  <li>
+                    <b>Click</b> the chart series to zoom to the respective stations.
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div className="informationDiv" hidden></div>
+            )}
+          </CalcitePanel>
+        </CalciteShellPanel>
+        <div className="mapDiv" ref={mapDiv}></div>
+      </CalciteShell>
     </div>
   );
 }
---
+++ src/Scene.ts
@@ -0,0 +1,102 @@
+import SceneView from '@arcgis/core/views/SceneView';
+import Map from '@arcgis/core/Map';
+import BasemapGallery from '@arcgis/core/widgets/BasemapGallery';
+import LayerList from '@arcgis/core/widgets/LayerList';
+import Search from '@arcgis/core/widgets/Search';
+import Expand from '@arcgis/core/widgets/Expand';
+import GroupLayer from '@arcgis/core/layers/GroupLayer';
+
+import {
+  chainageLayer,
+  stationBoxLayer,
+  stationLayer,
+  prowLayer,
+  monitorPointLayer,
+} from './layers';
+
+export const map = new Map({
+  basemap: 'dark-gray-vector', // "streets-night-vector", basemap
+  ground: 'world-elevation',
+});
+
+// Group layers //
+const alignmentGroupLayer = new GroupLayer({
+  title: 'Alignment',
+  visible: true,
+  visibilityMode: 'independent',
+  layers: [stationBoxLayer, chainageLayer, prowLayer],
+});
+
+// Change the layer order by using index numbers in map.add
+map.add(alignmentGroupLayer);
+map.add(stationLayer);
+map.add(monitorPointLayer);
+
+export const view = new SceneView({
+  container: undefined,
+  map,
+  camera: {
+    position: {
+      x: 121.005,
+      y: 14.56,
+      z: 3500,
+    },
+    tilt: 65,
+  },
+  viewingMode: 'local',
+});
+
+export const basemaps = new BasemapGallery({
+  view,
+  container: undefined,
+});
+
+// highlight super urgent
+export const layerList = new LayerList({
+  view: view,
+  selectionEnabled: true,
+  container: undefined,
+  listItemCreatedFunction: (event) => {
+    const item = event.item;
+    if (item.layer.type !== 'group') {
+      item.panel = {
+        content: 'legend',
+        open: true,
+      };
+    }
+
+    item.title === 'Chainage' ? (item.visible = false) : (item.visible = true);
+  },
+});
+
+const sources = [
+  {
+    layer: chainageLayer,
+    searchFields: ['KmSpot'],
+    displayField: 'KmSpot',
+    exactMatch: false,
+    outFields: ['*'],
+    zoomScale: 1000,
+    name: 'Main KM',
+    placeholder: 'example: 80 + 400',
+  },
+];
+
+const searchWidget = new Search({
+  view: view,
+  locationEnabled: false,
+  allPlaceholder: 'Chainage',
+  includeDefaultSources: false,
+  container: undefined,
+  sources: sources,
+});
+
+const searchExpand = new Expand({
+  view: view,
+  content: searchWidget,
+  expandIconClass: 'esri-icon-search',
+  group: 'top-right',
+});
+view.ui.add(searchExpand, {
+  position: 'top-right',
+});
